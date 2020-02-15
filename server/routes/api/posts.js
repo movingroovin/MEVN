7d493e4b05b8774d1a37c92d27a27c2fb85fa40f
movingroovin <finnbloo@gmail.com>
2020-02-15 17:06:55 +0800

    server api

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollections()
+  res.send(await posts.find({}).toArray())
+})
+
+// Add Posts
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollections()
+  await posts.insertOne({
+    text: req.body.text,
+    createTime: new Date()
+  })
+  res.status(201).send('Add successfully');
+})
+
+// Delete Posts
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollections()
+  await posts.deleteOne({
+    _id: new mongodb.ObjectID(req.params.id)
+  })
+  res.status(200).send('Deleted');
+})
+
+// connection to mongodb
+async function loadPostsCollections () {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb://wei:wei820801@ds045632.mlab.com:45632/mevn', {
+      useNewUrlParser: true
+    }
+  )
+  return client.db('mevn').collection('posts')
+}
+
+module.exports = router
