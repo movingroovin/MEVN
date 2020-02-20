79651559108e83a193681aa12fe6a3deb16c3f53
movingroovin <finnbloo@gmail.com>
2020-02-21 01:43:15 +0800

    use Carousel to test (style still needs to revise)

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -1,10 +1,37 @@
 <template>
+<div >
+  <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets/pics/carousel1.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/pics/carousel2.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/pics/carousel3.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div>
       <h4>Say something</h4>
       <input type="text" v-model="text">
-      <button @click="createPost">Submit</button>
+      <button type="button" class="btn btn-outline-primary" @click="createPost">Submit</button>
     </div>
     <hr>
     <div v-for="(post, index) in posts" :key="index">
@@ -13,6 +40,7 @@
       <button @click="deletePost(post._id)">Delete</button>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -67,5 +95,14 @@ li {
 }
 a {
   color: #42b983;
+}
+.carousel-item{
+    height: 800px;
+} 
+.carousel-item img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-height: 500px;
 }
 </style>
