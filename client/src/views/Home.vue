74b58537c2f96581aff69a80fe24fe60fde48949
movingroovin <finnbloo@gmail.com>
2020-02-23 01:55:06 +0800

    reponsive caption

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
 <div>
-  <nav class="navbar navbar-light bg-light">
+  <!--<nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="#">Oceanice</a>
-  </nav>
+  </nav>-->
   <div class="row">
     <div class="col-md-12">
-      <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+      <div id="carouselIndicators" class="carousel slide " data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselIndicators" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img src="../assets/pics/carousel1.jpg" class="d-block img-fluid" alt="...">
+            <div class="carousel-caption">
+              <h1>
+                <span class="d-none d-sm-block">極凍，大海的饗宴</span>
+                <span class="d-block d-sm-none">凍洋</span>
+              </h1>
+              <p class="d-none d-lg-block">凍洋致力於將海中最極致的鮮美原封不動地呈現在您的餐桌上。漁產從離開海面起，便進入極低溫環境運作。從加工，封裝，到運送。完整地將大海的鮮美冰封保存，直到您的餐桌前重現。</p>
+              <p class="d-none d-lg-block">不僅急凍，更是極凍。</p>
+            </div>
           </div>
           <div class="carousel-item">
             <img src="../assets/pics/carousel2.jpg" class="d-block img-fluid" alt="...">
@@ -22,11 +30,11 @@
             <img src="../assets/pics/carousel3.jpg" class="d-block img-fluid" alt="...">
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a class="carousel-control-prev d-none d-sm-flex" href="#carouselIndicators" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a class="carousel-control-next d-none d-sm-flex" href="#carouselIndicators" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
@@ -104,7 +112,7 @@ a {
   color: #42b983;
 }
 .carousel {
-  height: 75%;
+  height: 70%;
 }
 .carousel-inner {
   height: 100%;
@@ -113,5 +121,8 @@ a {
   height: 100%;
   background-size: cover;
   background-position: center;
+}
+.carousel-caption {
+  height: 57%;
 }
 </style>
