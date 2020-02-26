4518b00000ab1db38975ee9cb9cbb984cc099a41
movingroovin <finnbloo@gmail.com>
2020-02-27 01:04:29 +0800

    add product pics

---
+++ client/src/views/Product/Home.vue
@@ -0,0 +1,76 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/厚切鮭魚.jpg" class="card-img-top" alt="厚切鮭魚">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/土魠魚排.jpg" class="card-img-top" alt="土魠魚排">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/生食干貝.jpg" class="card-img-top" alt="生食干貝">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/金目鱸魚.jpg" class="card-img-top" alt="金目鱸魚">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/鮮凍透抽.jpg" class="card-img-top" alt="鮮凍透抽">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+            <img src="./../../assets/pics/Products/龍虎石斑.jpg" class="card-img-top" alt="龍虎石斑">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
